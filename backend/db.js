29b3573c5d24de153963bd3197cdc18719fc06e3
dante21196 <ashminsingh21196@gmail.com>
2023-06-05 03:09:47 +0530

    server setup backend initial

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://127.0.0.1:27017/myapp";
+
+const connectToMongo = async() => {
+  await mongoose.connect(mongoURI,{
+    
+  });
+  console.log("Connected to Mongo Successfully")
+  }
+  
+
+
+module.exports = connectToMongo;
